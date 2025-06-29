d37f4919d915691a551afad7c0a0639413a688a3
Tim Illguth <entrepreneurracer@gmail.com>
2025-06-29 17:05:40 -0500

    Added icon color for dark and light modes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -63,7 +63,7 @@ const Header: React.FC = () => {
               onClick={toggleTheme}
               className="p-2 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200"
             >
-              {isDark ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
+              {isDark ? <Sun className="w-5 h-5 text-white" /> : <Moon className="w-5 h-5 text-black" />}
             </button>
 
             {isConnected ? (
@@ -133,7 +133,7 @@ const Header: React.FC = () => {
                   onClick={toggleTheme}
                   className="p-2 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200"
                 >
-                  {isDark ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
+                  {isDark ? <Sun className="w-5 h-5 text-white" /> : <Moon className="w-5 h-5 text-black" />}
                 </button>
 
                 {isConnected ? (
